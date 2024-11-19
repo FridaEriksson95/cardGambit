1ab10a2fa7067dca67db8e5e768a4e9962f9dbae
Simon-E-Mapp <3ggapp24.baugsi@folkuniversitetet.nu>
2024-11-19 14:32:03 +0100

    Front Page done

--- app/src/main/java/com/example/cardgambit/StartGame.kt
+++ app/src/main/java/com/example/cardgambit/StartGame.kt
@@ -1,11 +1,11 @@
 package com.example.cardgambit
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.cardgambit.databinding.ActivityMainBinding
 import com.example.cardgambit.databinding.ActivityStartGameBinding
 
 class StartGame : AppCompatActivity() {
@@ -23,6 +23,10 @@ class StartGame : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnFrontPage.setOnClickListener{
+            val newIntent = Intent(this, FrontPage::class.java)
+            startActivity(newIntent)
         }
     }
 }
