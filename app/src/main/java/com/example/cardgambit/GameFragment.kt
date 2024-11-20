34071afde206a78aeb451942c325288a25bfdba8
FridaEriksson95 <fridaeriiksson@gmail.com>
2024-11-20 23:32:04 +0100

    fixed design on startgame xml and added the images of cards.

--- app/src/main/java/com/example/cardgambit/GameFragment.kt
+++ app/src/main/java/com/example/cardgambit/GameFragment.kt
@@ -8,6 +8,22 @@ import android.view.ViewGroup
 
 class GameFragment : Fragment() {
 
+    var cardsArray = intArrayOf(
+        R.drawable.two,
+        R.drawable.three,
+        R.drawable.four,
+        R.drawable.five,
+        R.drawable.six,
+        R.drawable.seven,
+        R.drawable.eight,
+        R.drawable.nine,
+        R.drawable.ten,
+        R.drawable.eleven,
+        R.drawable.twelve,
+        R.drawable.thirteen,
+        R.drawable.one
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
