400bde14a871b1231cf697c96b5673f84bed971d
FridaEriksson95 <fridaeriiksson@gmail.com>
2024-11-22 11:06:30 +0100

    Start of Game with many bugs.

--- app/src/main/java/com/example/cardgambit/GameFragment.kt
+++ app/src/main/java/com/example/cardgambit/GameFragment.kt
@@ -24,6 +24,37 @@ class GameFragment : Fragment() {
         R.drawable.one to 1
     )
 
+    var previousCardValue: Int = getRandomCardValue()
+    var player1score = 0
+    var player2score = 0
+
+    fun getRandomCard(): Pair<Int, Int> {
+        val randomCard = cardsMap.keys.random()
+        return Pair(randomCard, cardsMap[randomCard]!!)
+    }
+
+    private fun getRandomCardValue(): Int {
+        return cardsMap.values.random()
+    }
+
+    fun isGuessCorrect(newCardValue: Int, guessHigher: Boolean): Boolean {
+        return if (guessHigher) {
+            newCardValue > previousCardValue
+        } else {
+            newCardValue < previousCardValue
+        }
+    }
+
+    fun updateScore(player: Int, correct: Boolean) {
+        if (correct) {
+            if (player == 1) player1score++ else player2score++
+        }
+    }
+
+    fun updatePreviousCardValue(newValue: Int) {
+        previousCardValue = newValue
+    }
+
    // val randomCard = cardsMap.keys.random() // Slumpa fram en drawable
    // val cardValue = cardsMap[randomCard]   // Hämta det numeriska värdet
 
